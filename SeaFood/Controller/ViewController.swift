099e3f77fa5bcbf8d371e31ac915872372949be8
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-11 19:41:55 -0700

    Import the Image Recognition Model & Enable Camera Functionality

---
+++ SeaFood/Controller/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Philip Yu on 7/11/19.
+//  Copyright © 2019 Philip Yu. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    // Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // Properties
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
